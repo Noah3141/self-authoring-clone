d4ee03e910fc5d454cf4e2adc200a8945e3baa74
Noah Steckley <Noah3141@gmail.com>
2024-04-11 15:55:57 -0700

    Stupid

--- src/components/Containers/Footer/index.tsx
+++ src/components/Containers/Footer/index.tsx
@@ -1,51 +1,51 @@
-import type { FC, SVGProps } from "react";
+// import type { FC, SVGProps } from "react";
 
-import styles from "./index.module.css";
-import LinkedIn from "../Icons/Social/LinkedIn";
-import FooterItem, { type FooterItemProps } from "./FooterItem";
+// import styles from "./index.module.css";
+// import FooterItem, { type FooterItemProps } from "./FooterItem";
+// import LinkedIn from "~/components/Icons/Social/LinkedIn";
 
-const footerItems: Record<string, FooterItemProps> = {
-    linkedin: {
-        icon: LinkedIn,
-        link: "https://www.linkedin.com/in/noah-steckley/",
-        text: "Noah Steckley",
-    },
-};
+// const footerItems: Record<string, FooterItemProps> = {
+//     linkedin: {
+//         icon: LinkedIn,
+//         link: "https://www.linkedin.com/in/noah-steckley/",
+//         text: "Noah Steckley",
+//     },
+// };
 
-const footerLinks: Record<string, FooterLink> = {};
+// const footerLinks: Record<string, FooterLink> = {};
 
-const Footer: FC = () => {
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.sectionPrimary}>
-                {footerItems.map((item) => (
-                    <FooterItem href={item.link} key={item.link}>
-                        {item.text}
-                    </FooterItem>
-                ))}
-            </div>
+// const Footer: FC = () => {
+//     return (
+//         <footer className={styles.footer}>
+//             <div className={styles.sectionPrimary}>
+//                 {footerItems.map((item) => (
+//                     <FooterItem href={item.link} key={item.link}>
+//                         {item.text}
+//                     </FooterItem>
+//                 ))}
+//             </div>
 
-            <div className={styles.sectionSecondary}>
-                <div className={styles.social}>
-                    {footerLinks.map((link) => {
-                        return (
-                            <FooterItem
-                                key={link.icon}
-                                href={link.link}
-                                type="footer"
-                            >
-                                <SocialIcon
-                                    width={20}
-                                    height={20}
-                                    aria-label={link.link}
-                                />
-                            </FooterItem>
-                        );
-                    })}
-                </div>
-            </div>
-        </footer>
-    );
-};
+//             <div className={styles.sectionSecondary}>
+//                 <div className={styles.social}>
+//                     {footerLinks.map((link) => {
+//                         return (
+//                             <FooterItem
+//                                 key={link.icon}
+//                                 href={link.link}
+//                                 type="footer"
+//                             >
+//                                 <SocialIcon
+//                                     width={20}
+//                                     height={20}
+//                                     aria-label={link.link}
+//                                 />
+//                             </FooterItem>
+//                         );
+//                     })}
+//                 </div>
+//             </div>
+//         </footer>
+//     );
+// };
 
-export default Footer;
+// export default Footer;
